d6d928f29ca4f90ffd83894df4401b4387a30985
WeijieChen2017 <weijiechen1994@gmail.com>
2020-11-16 12:47:49 -0600

    v1

--- C_nifty2nifty.py
+++ C_nifty2nifty.py
@@ -4,12 +4,12 @@ import numpy as np
 import nibabel as nib
 import glob
 
-leah_list = glob.glob("./data/pet/*.nii")
+leah_list = glob.glob("../../BraTS20T_001_039/*.nii.gz")
 for leah_name in leah_list:
     name = leah_name
     data = nib.load(leah_name).get_fdata()
 
-    tmpl_name = "./data/mri_89.nii"
+    tmpl_name = "./data/example.nii"
     file_nii = nib.load(tmpl_name)
     file_data = file_nii.get_fdata()
     file_header = file_nii.header
@@ -19,11 +19,11 @@ for leah_name in leah_list:
     # data[data>1] = 1
 
     px, py, pz = data.shape
-    qx, qy, qz = (512, 512, 89)
+    qx, qy, qz = (512, 512, 178)
     zoom_data = zoom(data, (qx/px, qy/py, qz/pz))
 
     print("Old dim:", data.shape)
     print("New dim:", zoom_data.shape)
 
     sino_file = nib.Nifti1Image(zoom_data, affine=file_affine, header=file_header)
-    nib.save(sino_file, name[:-4] +"_512.nii")
+    nib.save(sino_file, name[:-4] +"_xy512z178.nii")
