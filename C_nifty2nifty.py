db59d00fc1428b23eeb7a13e93d2fd2dc413a8ff
WeijieChen2017 <weijiechen1994@gmail.com>
2020-11-16 21:25:22 -0600

    v1

--- C_nifty2nifty.py
+++ C_nifty2nifty.py
@@ -21,22 +21,22 @@ for leah_name in leah_list:
     # data[data>1] = 1
 
     px, py, pz = data.shape
-    qx, qy, qz = (512, 512, 178)
+    qx, qy, qz = (px*2, py*2, pz*2)
 
-    zoom_data = zoom(data, (qx/px, qy/py, qz/pz))
+    zoom_data = zoom(data, (2, 2, 2))
     zoom_data = zoom_data / np.sum(zoom_data) * np.sum(data)
     print("Old dim:", data.shape)
     print("New dim:", zoom_data.shape)
     print("Original sum:", np.sum(data))
     print("2x sum:", np.sum(zoom_data))
 
-    PVE_data = np.zeros((256, 256, 89))
-    for idx in range(256):
-        for idy in range(256):
-            for idz in range(89):
-                print("Old sum:", data[idx, idy, idz])
+    PVE_data = np.zeros((px, py, pz))
+    for idx in range(px):
+        for idy in range(py):
+            for idz in range(pz):
+                # print("Old sum:", data[idx, idy, idz])
                 new_sum = np.sum(zoom_data[idx*2:idx*2+1, idy*2:idy*2+1, idz*2:idz*2+1])
-                print("New sum:", new_sum)
+                # print("New sum:", new_sum)
                 PVE_data[idx, idy, idz] = new_sum
 
     print("PVE sum:", np.sum(PVE_data))
