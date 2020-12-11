eb745cace79722a44fa72153161a82926413e3f6
WeijieChen2017 <weijiechen1994@gmail.com>
2020-12-11 17:05:47 -0600

    whole BraTS20 Training dataset

--- A_process.py
+++ A_process.py
@@ -5,7 +5,7 @@ from scipy.ndimage import zoom
 import glob
 import os
 
-file_list = glob.glob("./BraTS20T_001_039/*.nii.gz")
+file_list = glob.glob("./BraTS20T/*.nii.gz")
 file_list.sort()
 for file_name in file_list:
     print(file_name)
@@ -23,7 +23,7 @@ for file_name in file_list:
     print("New dim:", zoom_data.shape)
 
     mdic = {"data": zoom_data}
-    savemat("./BraTS20T_001_039/"+os.path.basename(file_name)[:-7]+".mat", mdic)
+    savemat("./BraTS20T/"+os.path.basename(file_name)[:-7]+".mat", mdic)
 
     # pure_file = nib.Nifti1Image(zoom_data, affine=file_affine, header=file_header)
     # nib.save(pure_file, "./BraTS20T_001_039/"+os.path.basename(file_name)[:-4]+".nii")
