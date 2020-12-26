4f2149e6e59607990b6b080179a63d96ba6cfa82
WeijieChen2017 <weijiechen1994@gmail.com>
2020-12-25 19:06:05 -0600

    Matthew and Tim

---
+++ AA_nifty2mat.py
@@ -0,0 +1,32 @@
+from scipy.ndimage import binary_erosion, binary_dilation
+from scipy.io import savemat
+from nibabel import processing
+import nibabel as nib
+import numpy as np
+import glob
+import os
+
+nii_list = glob.glob("./inv_RSZP/*.nii")
+nii_list.sort()
+for nii_path in nii_list:
+    print(nii_path)
+    nii_file = nib.load(nii_path)
+    nii_data = nii_file.get_fdata()
+    print(nii_data.shape)
+    
+    mdic = {"data": nii_data}
+    nii_name = os.path.basename(nii_path)
+    nii_group = nii_name[1:3]
+    save_name = "./inv_RSZP_mat/"+nii_group+"/"
+    if not os.path.exists(save_name):
+        os.makedirs(save_name)
+    save_name += nii_name[:-4]+".mat"
+    savemat(save_name, mdic)
+    print("Mat:", save_name)
+
+    save_file = nib.Nifti1Image(norm_data, affine=nii_file.affine, header=nii_file.header)
+    smoothed_file = processing.smooth_image(save_file, fwhm=3, mode='nearest')
+    save_name = "./inv_RSZP_f3/"+nii_name
+    nib.save("F3:", smoothed_file, save_name)
+    print(save_name)
+    print("--------------------------------------")
