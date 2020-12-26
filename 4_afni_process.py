1e3e28bf0bd19856eaf49002244f350c15fc36d8
WeijieChen2017 <weijiechen1994@gmail.com>
2020-12-25 18:16:08 -0600

    Matthew and Tim

--- 4_afni_process.py
+++ 4_afni_process.py
@@ -7,8 +7,8 @@ for idx in range(369):
 	cmd_2 = "3dZeropad -I 16 -S 17 -A 25 -P 26 -L 25 -R 26 z"+idx_str+"+orig"
 	cmd_3 = "3dAFNItoNIFTI -prefix z"+idx_str+" zeropad+orig"
 	cmd_4 = "rm -f zeropad+orig.BRIK"
-	cmd_5 = "rm -f zeropad+orih.HEAD"
-	cmd_6 = "mv z"+idx_str+" ../inv_RSZP"
+	cmd_5 = "rm -f zeropad+orig.HEAD"
+	cmd_6 = "mv z"+idx_str+".nii ../inv_RSZP"
 	for cmd in [cmd_1, cmd_2, cmd_3, cmd_4, cmd_5, cmd_6]:
 		os.system(cmd)
 # 3dresample -dxyz 1.172 1.172 2.78 -prefix test -inset BraTS20_Training_001_t1_inv.nii
