2fd9d14a51395977a6862cb4f20a2e197531e699
WeijieChen2017 <weijiechen1994@gmail.com>
2020-12-27 04:01:11 -0600

    Gibbs recon

--- C_nifty2mat.py
+++ C_nifty2mat.py
@@ -6,7 +6,7 @@ import numpy as np
 import glob
 import os
 
-nii_list = glob.glob("./inv_RSZP/*.nii")
+nii_list = glob.glob("./HURLEY/*.nii")
 nii_list.sort()
 for nii_path in nii_list:
     print(nii_path)
@@ -17,19 +17,19 @@ for nii_path in nii_list:
     mdic = {"data": nii_data}
     nii_name = os.path.basename(nii_path)
     nii_group = nii_name[1:3]
-    save_name = "./RSZP_mat/"+nii_group+"/"
+    save_name = "./HURLEY_mat/"+nii_group+"/"
     if not os.path.exists(save_name):
         os.makedirs(save_name)
     save_name += nii_name[:-4]+".mat"
     savemat(save_name, mdic)
     print("Mat:", save_name)
 
-    save_name = "./RSZP_f3/"
+    save_name = "./HURLEY_F3/"
     if not os.path.exists(save_name):
         os.makedirs(save_name)
     save_file = nib.Nifti1Image(nii_data, affine=nii_file.affine, header=nii_file.header)
     smoothed_file = processing.smooth_image(save_file, fwhm=3, mode='nearest')
-    save_name = "./RSZP_f3/"+nii_name
+    save_name = "./HURLEY_F3/"+nii_name
     nib.save(smoothed_file, save_name)
     print("F3:", save_name)
     print("--------------------------------------")
