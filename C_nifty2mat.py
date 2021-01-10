f59d47441125d10c96243410c6c39c07930aba8b
WeijieChen2017 <weijiechen1994@gmail.com>
2021-01-09 21:34:36 -0600

    Gibbs recon

--- C_nifty2mat.py
+++ C_nifty2mat.py
@@ -6,7 +6,8 @@ import numpy as np
 import glob
 import os
 
-nii_list = glob.glob("./HURLEY/*.nii")
+tag = "B9"
+nii_list = glob.glob("./"+tag+"_inv/*.nii")
 nii_list.sort()
 for nii_path in nii_list:
     print(nii_path)
@@ -17,19 +18,19 @@ for nii_path in nii_list:
     mdic = {"data": nii_data}
     nii_name = os.path.basename(nii_path)
     nii_group = nii_name[1:3]
-    save_name = "./HURLEY_mat/"+nii_group+"/"
+    save_name = "./"+tag+"_mat/"+nii_group+"/"
     if not os.path.exists(save_name):
         os.makedirs(save_name)
     save_name += nii_name[:-4]+".mat"
     savemat(save_name, mdic)
     print("Mat:", save_name)
 
-    save_name = "./HURLEY_F3/"
+    save_name = "./"+tag+"_F3/"
     if not os.path.exists(save_name):
         os.makedirs(save_name)
     save_file = nib.Nifti1Image(nii_data, affine=nii_file.affine, header=nii_file.header)
     smoothed_file = processing.smooth_image(save_file, fwhm=3, mode='nearest')
-    save_name = "./HURLEY_F3/"+nii_name
+    save_name = "./"+tag+"_F3/"+nii_name
     nib.save(smoothed_file, save_name)
     print("F3:", save_name)
     print("--------------------------------------")
